932165c63519ab06c05cbadfb702d5bc58f2c5b2
Rian Almeida <rian.almeida@lpscompany.com.br>
2025-06-20 13:39:16 -0300

    Testando commit no novo repositório

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.concurrent.ConcurrentMap;
-
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
